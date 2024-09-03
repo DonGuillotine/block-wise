45adc7ef4dfe699c80f12820703c61d96f4169f0
signor1 <emmanuelomemgboji@gmail.com>
2024-09-03 18:54:36 +0100

    feat: configured web3modal

---
+++ frontend/config/index.tsx
@@ -0,0 +1,27 @@
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { cookieStorage, createStorage } from 'wagmi'
+import { rootstockTestnet } from 'wagmi/chains'
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+export const metadata = {
+    name: 'ChainLearn',
+    description: 'An AI based learning platform to help you learn new skills and concepts',
+    url: 'https://web3modal.com', // origin must match your domain & subdomain
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// Create wagmiConfig
+const chains = [rootstockTestnet] as const
+export const config = defaultWagmiConfig({
+    chains,
+    projectId,
+    metadata,
+    ssr: true,
+    storage: createStorage({
+        storage: cookieStorage
+    }),
+})
